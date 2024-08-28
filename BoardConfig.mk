1b78884653abc9f88b5659177ef9b7b245ba13e4
LuK1337 <priv.luk@gmail.com>
2024-08-28 15:32:59 +0200

    a42xq: Migrate to soong_config_set()
    Change-Id: I06de8759836114f54a822d6f23f1177515e21178

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,11 +23,7 @@ TARGET_KERNEL_CONFIG        := vendor/a42xq_eur_open_defconfig
 BOARD_NAME                  := SRPTF23D003
 
 # Camera
-SOONG_CONFIG_NAMESPACES += samsungCameraVars
-SOONG_CONFIG_samsungCameraVars += extra_ids
-# ID=50 is wide camera
-# ID=54 is macro
-SOONG_CONFIG_samsungCameraVars_extra_ids := 50,54
+$(call soong_config_set,samsungCameraVars,extra_ids,50,54) # ID=50 is depth camera and ID=54 is macro
 
 # Display
 TARGET_SCREEN_DENSITY := 300
